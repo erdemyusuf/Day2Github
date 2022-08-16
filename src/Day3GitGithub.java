ed1dff214e43e9c0d071f0c4702e2c2d6ad509cc
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-16 21:31:05 +0300

    ysf branch

--- src/Day3GitGithub.java
+++ src/Day3GitGithub.java
@@ -2,6 +2,10 @@ public class Day3GitGithub {
     public static void main(String[] args) {
         System.out.println("3. gun denemesi");
         System.out.println("3. gun branch denemesi");
+        System.out.println("3. gun 2 branch denemesi");
+
+
+
 
     }
 }
