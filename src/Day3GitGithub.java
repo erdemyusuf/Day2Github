02df3c2a5abe881586ab630af178261166a668a4
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-17 10:33:35 +0300

    dghdfglk

--- src/Day3GitGithub.java
+++ src/Day3GitGithub.java
@@ -2,5 +2,9 @@ public class Day3GitGithub {
 
     public static void main(String[] args) {
         System.out.println("branch calısmasi");
+        System.out.println("branch calısmasi");
+        System.out.println("branch calısmasi");
+        System.out.println("branch calısmasi");
+        System.out.println("branch calısmasi");
     }
 }
