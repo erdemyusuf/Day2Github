b3e33e0b193469b36a713efc2c432e792cc5c9c7
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-17 11:04:27 +0300

    eee

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -5,5 +5,6 @@ public class Day1Git {
         System.out.println("Hello Git 2. Deneme");
         System.out.println("Hello Git 3. Deneme");
         System.out.println("Hello Git 4. Deneme");
+        System.out.println("hellooooo");
     }
 }
